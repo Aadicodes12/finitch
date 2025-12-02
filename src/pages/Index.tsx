6ce56cb203bba55ed0e781669dd6bda9ad51a632
[dyad] <aadityawaykole@gmail.com>
2025-12-02 15:37:29 +0530

    [dyad] Removed all text from Index page - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,10 +5,7 @@ import React from "react";
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4">
-      <h1 className="text-4xl font-bold mb-4">Hello, Dyad!</h1>
-      <p className="text-lg text-gray-600 mb-8">
-        Your application is ready. Let's build something amazing!
-      </p>
+      {/* All text content has been removed */}
     </div>
   );
 };
