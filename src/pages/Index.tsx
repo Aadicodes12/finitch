b405c0882eaec2534b9854cc93c7a5b0cea7d38b
[dyad] <aadityawaykole@gmail.com>
2025-12-02 17:02:33 +0530

    [dyad] Adjusted waitlist form vertical position for mobile screens - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,11 +7,11 @@ const Index = () => {
   return (
     <div className="min-h-screen w-screen flex items-center justify-center bg-white relative overflow-hidden">
       <img
-        src="/finitch-landing.png" // Updated to use the new image name
+        src="/finitch-landing.png"
         alt="Finitch Landing Page"
         className="w-full h-full object-cover absolute inset-0"
       />
-      <div className="absolute top-[43%] left-1/2 -translate-x-[43%] -translate-y-1/2 w-full px-4 md:left-[34%] md:translate-x-0">
+      <div className="absolute top-[50%] left-1/2 -translate-x-[43%] -translate-y-1/2 w-full px-4 md:top-[43%] md:left-[34%] md:translate-x-0">
         <WaitlistForm />
       </div>
     </div>
