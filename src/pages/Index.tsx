cec30bfd739c78632b76ef0f8fa0d595cbd47302
[dyad] <aadityawaykole@gmail.com>
2025-12-02 21:04:05 +0530

    [dyad] Moved the waitlist form to top-43 position - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -18,7 +18,7 @@ const Index = () => {
         />
 
         {/* Adjusted positioning for the WaitlistForm to align with the "Get on our waitlist now!" text in the image */}
-        <div className="absolute top-[45%] left-1/2 -translate-x-1/2 w-full px-4 flex justify-center z-10">
+        <div className="absolute top-[43%] left-1/2 -translate-x-1/2 w-full px-4 flex justify-center z-10">
           <WaitlistForm />
         </div>
       </div>
