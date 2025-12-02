b4de2fd8ec0a6c130241832171398cf0e8432941
[dyad] <aadityawaykole@gmail.com>
2025-12-02 17:01:33 +0530

    Reverted all changes back to version d7a8b8b5c67ecce2b2c5e0ca7dda01d8893e70e2

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,11 +7,11 @@ const Index = () => {
   return (
     <div className="min-h-screen w-screen flex items-center justify-center bg-white relative overflow-hidden">
       <img
-        src="/finitch-landing.png"
+        src="/finitch-landing.png" // Updated to use the new image name
         alt="Finitch Landing Page"
-        className="w-full h-full object-contain absolute inset-0" // Changed to object-contain
+        className="w-full h-full object-cover absolute inset-0"
       />
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full px-4 max-w-sm md:left-[15%] md:translate-x-0">
+      <div className="absolute top-[43%] left-1/2 -translate-x-[43%] -translate-y-1/2 w-full px-4 md:left-[34%] md:translate-x-0">
         <WaitlistForm />
       </div>
     </div>
