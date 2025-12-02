c7b2d449047d88f3e0cce95f9aede76858cfba61
[dyad] <aadityawaykole@gmail.com>
2025-12-02 17:04:44 +0530

    [dyad] wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,13 +5,13 @@ import WaitlistForm from "@/components/WaitlistForm";
 
 const Index = () => {
   return (
-    <div className="min-h-screen w-screen flex items-center justify-center bg-white relative overflow-hidden">
+    <div className="min-h-screen w-screen flex flex-col items-center justify-start bg-white relative">
       <img
-        src="/finitch-landing.png" // Updated to use the new image name
+        src="/finitch-landing.png"
         alt="Finitch Landing Page"
-        className="w-full h-full object-cover absolute inset-0"
+        className="w-full h-auto" // Image will scale to full width, height auto, maintaining aspect ratio
       />
-      <div className="absolute top-[43%] left-1/2 -translate-x-[43%] -translate-y-1/2 w-full px-4 md:left-[34%] md:translate-x-0">
+      <div className="absolute top-[50%] left-1/2 -translate-x-[43%] -translate-y-1/2 w-full px-4 md:top-[43%] md:left-[34%] md:translate-x-0">
         <WaitlistForm />
       </div>
     </div>
